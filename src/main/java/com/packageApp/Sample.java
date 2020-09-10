090261760498f762aaa135660ee44d0df2804eb6
IST@ <viswarupaachari88@gmail.com>
2020-09-10 13:06:31 +0530

    Modified to changing tomcat server port using application.properties

--- src/main/java/com/packageApp/Sample.java
+++ src/main/java/com/packageApp/Sample.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.packageController")
+@ComponentScan(basePackages  = "com.packageServer")
 public class Sample {
 
 	public static void main(String[] args) {
--- src/main/java/com/packageApp/SampleRest.java
+++ src/main/java/com/packageApp/SampleRest.java
@@ -2,8 +2,10 @@ package com.packageApp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class SampleRest {
+
 	@RequestMapping("/")
 	public String welcome() {
 		return "Hai welcome";
